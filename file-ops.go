db993739ef917a00ee99156fb69372dfe2836fce
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-01-20 02:27:00 +0000

    feat(file-ops): extend error reports, edit comments

--- file-ops.go
+++ file-ops.go
@@ -5,54 +5,64 @@ import (
 	"os"
 )
 
-// Read a data from file, convert every its bit with XOR by a key, and
-// then replace initial file content with the result.
-// Returns number of encrypted bytes and error (if appeared).
+const (
+	errorExecReadingSeq     = "could not execute reading sequence"
+	errorExecOverwritingSeq = "could not execute overwriting sequence"
+	errorOpenFile           = "could not open file"
+	errorReadFile           = "could not read from file"
+	errorWriteFile          = "could not write to file"
+)
+
+// EncryptFile performs per-bit XOR data encryption by the key with the
+// file by its path. The same key must be used for the decryption.
+// Returns the number of the encrypted bytes and the error with its
+// description.
 func EncryptFile(path string, key []byte) (int, error) {
 	data, err := readFile(path)
 	if err != nil {
-		return 0, decorateError("could not read file", err)
+		return 0, decorateError(errorExecReadingSeq, err)
 	}
 
 	nbytes := EncryptData(data, key)
 
 	err = overwriteFile(path, data)
 	if err != nil {
-		return 0, decorateError("could not overwrite file", err)
+		return 0, decorateError(errorExecOverwritingSeq, err)
 	}
 
 	return nbytes, nil
 }
 
-// Read data from file.
-// Returns file data and error (if appeared).
+// readFile reads the data from a file by its path.
+// Returns the file data and the error with its description.
 func readFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, decorateError(errorOpenFile, err)
 	}
 	defer f.Close()
 
 	data, err := io.ReadAll(f)
 	if err != nil {
-		return nil, err
+		return nil, decorateError(errorReadFile, err)
 	}
 
 	return data, nil
 }
 
-// Overwrite whole file, if exists, or create new file with the data.
-// Returns error (if appeared).
+// overwriteFile overwrites the whole file by its path with the data. If
+// the file does not exists yet, it creates a new one.
+// Returns an error with its description.
 func overwriteFile(path string, data []byte) error {
 	f, err := os.OpenFile(path, os.O_TRUNC|os.O_WRONLY, 0)
 	if err != nil {
-		return err
+		return decorateError(errorOpenFile, err)
 	}
 	defer f.Close()
 
 	_, err = f.Write(data)
 	if err != nil {
-		return err
+		return decorateError(errorWriteFile, err)
 	}
 
 	return nil
