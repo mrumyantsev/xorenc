4d8bc6e233e5f3924a7de44c1715b89512f3d7fe
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-01-20 13:52:18 +0000

    refactor(lib): remove nil check in decorateError function

--- lib.go
+++ lib.go
@@ -6,15 +6,10 @@ const (
 	errorInsert string = ". error: "
 )
 
-// decorateError decorates a given error with a given description,
-// inserting ". error: " text at the end of the description.
+// decorateError decorates a given error with the description, inserting
+// ". error: " text in between. If the error is nil, is cause panic.
 // Returns the new error, which text is decorated by the specified
-// description. If the given error is nil, then the returning error will
-// only contain the description text.
+// description.
 func decorateError(desc string, err error) error {
-	if err != nil {
-		desc += errorInsert + err.Error()
-	}
-
-	return errors.New(desc)
+	return errors.New(desc + errorInsert + err.Error())
 }
