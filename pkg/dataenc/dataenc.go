c1a89415237b70c52e5cf66ca8c0f16e99696236
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-01-21 18:44:16 +0000

    feat(dataenc): add concurrent data processing

--- pkg/dataenc/dataenc.go
+++ pkg/dataenc/dataenc.go
@@ -1,5 +1,7 @@
 package dataenc
 
+import "sync"
+
 // A DataEnc structure is responsible for encryption of data in terms of
 // XOR data conversion.
 type DataEnc struct {
@@ -12,27 +14,94 @@ func New() *DataEnc {
 }
 
 // Encrypt performs per-bit XOR encryption of data by the key.
+//
 // The decryption is provided by repeating of encryption with the same
 // key, that was in use in encryption.
+//
+// The data processing operation in this function is processed with the
+// workers, which divides the data to the equal pieces and encrypts
+// each. The number of workers is capped to the length of the data, and
+// it makes no processing, if given 0 or less.
+//
 // Returns the number of the encrypted bytes.
-func (e *DataEnc) Encrypt(data []byte, key []byte) (nBytes int) {
+func (e *DataEnc) Encrypt(data []byte, key []byte, nWorkers int) (nBytes int) {
+	if nWorkers <= 0 {
+		return 0
+	}
+
+	var dataLen int = len(data)
+
+	if nWorkers > dataLen {
+		nWorkers = dataLen
+	}
+
 	var (
-		dLen int = len(data) // data length
-		kLen int = len(key)  // key length
-		i    int = 0         // data index
-		j    int = 0         // key index
+		keyLen    int = len(key)
+		dataPart  int = dataLen / nWorkers
+		dataStart int = 0
+		dataEnd   int = dataPart
+		keyStart  int = 0
+
+		wg sync.WaitGroup = sync.WaitGroup{}
 	)
 
-	for i < dLen {
-		if j >= kLen {
+	wg.Add(nWorkers)
+
+	for i := int(0); i < nWorkers; i++ {
+		if i == (nWorkers - 1) {
+			dataEnd = dataLen
+		}
+
+		go worker(workerData{
+			data:      data,
+			key:       key,
+			keyStart:  keyStart,
+			dataStart: dataStart,
+			dataEnd:   dataEnd,
+			wg:        &wg,
+		})
+
+		dataStart += dataPart
+		dataEnd += dataPart
+		keyStart += dataPart
+
+		for keyStart >= keyLen {
+			keyStart -= keyLen
+		}
+	}
+
+	wg.Wait()
+
+	return dataLen
+}
+
+func worker(wd workerData) {
+	var (
+		keyLen  int = len(wd.key)
+		dataLen int = wd.dataEnd
+		i       int = wd.dataStart // data index
+		j       int = wd.keyStart  // key index
+	)
+
+	for i < dataLen {
+		if j >= keyLen {
 			j = 0
 		}
 
-		data[i] = data[i] ^ key[j]
+		wd.data[i] = wd.data[i] ^ wd.key[j]
 
 		i++
 		j++
 	}
 
-	return dLen
+	wd.wg.Done()
+}
+
+type workerData struct {
+	data      []byte
+	key       []byte
+	keyStart  int
+	dataStart int
+	dataEnd   int
+	wg        *sync.WaitGroup
 }
