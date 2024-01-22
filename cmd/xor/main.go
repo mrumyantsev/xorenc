937375c9587fa5c73b306bbb87ab137fa88d8151
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-01-22 01:15:18 +0000

    feat: add stdin(pipe)-to-stdout encrypting mode

--- cmd/xor/main.go
+++ cmd/xor/main.go
@@ -2,43 +2,107 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/mrumyantsev/xor"
 )
 
-const errorExitCode int = 1
+const (
+	errorExitCode int    = 1
+	errorInsert   string = ". error: "
+	appMark       string = "xor: "
+	usageInfo     string = "Usage:\n  - xor <filepath> <enckey>\n" +
+		"  - cat ./def_file | xor <enckey> > ./out_file\n" +
+		"  - xor <enckey> < ./def_file > ./out_file\n\n" +
+		"The encryption key (<enckey>) may contain spaces.\n\n" +
+		"The decryption is provided by repeating of encryption\n" +
+		"with the same key, that was in use in encryption."
+	space string = " "
+	eol   string = "\n"
+)
 
-func main() {
+var (
+	flagArgs      []string
+	stdinFileInfo fs.FileInfo
+	stdinFileMode fs.FileMode
+	nCores        int = runtime.NumCPU()
+	err           error
+)
+
+func init() {
 	flag.Parse()
 
-	var args []string = flag.Args()
+	flagArgs = flag.Args()
 
-	if len(args) < 2 {
-		fmt.Println("usage: xor <path/to/file> <any number of words as an encryption key>")
-		errorExit()
+	stdinFileInfo, err = os.Stdin.Stat()
+	if err != nil {
+		reportError("could not get stdin file info", err)
+	}
+
+	stdinFileMode = stdinFileInfo.Mode()
+}
+
+func main() {
+	// Encrypt the data from stdin to stdout.
+
+	if (stdinFileMode & os.ModeCharDevice) == 0 {
+		if len(flagArgs) < 1 {
+			reportWrongUsage("encryption key parameter does not presents")
+		}
+
+		var (
+			encryptKey []byte = []byte(strings.Join(flagArgs, space))
+			stdinData  []byte
+		)
+
+		stdinData, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			reportError("could not read data from stdin", err)
+		}
+
+		xor.EncryptData(stdinData, encryptKey, nCores)
+
+		_, err = os.Stdout.Write(stdinData)
+		if err != nil {
+			reportError("could not write encrypted data to stdout", err)
+		}
+
+		return
+	}
+
+	// Encrypt the file, given in file path parameter.
+
+	if len(flagArgs) < 2 {
+		reportWrongUsage("file path or encryption key is missing " +
+			"in parameters")
 	}
 
 	var (
-		filePath   string = args[0]
-		encryptKey []byte = []byte(strings.Join(args[1:], " "))
-		nCores     int    = runtime.NumCPU()
-		nBytes     int    = 0
-		err        error  = nil
+		filePath   string = flagArgs[0]
+		encryptKey []byte = []byte(strings.Join(flagArgs[1:], space))
+		nBytes     int
 	)
 
 	nBytes, err = xor.EncryptFile(filePath, encryptKey, nCores)
 	if err != nil {
-		fmt.Println(err.Error())
-		errorExit()
+		reportError("could not encrypt file", err)
 	}
 
-	fmt.Println("encrypted", nBytes, "bytes")
+	os.Stdout.WriteString(strconv.Itoa(nBytes) + " bytes encrypted" + eol)
 }
 
-func errorExit() {
+func reportError(desc string, err error) {
+	os.Stderr.WriteString(appMark + desc + errorInsert + err.Error() + eol)
+	os.Exit(errorExitCode)
+}
+
+func reportWrongUsage(desc string) {
+	os.Stderr.WriteString(appMark + desc + eol)
+	os.Stderr.WriteString(usageInfo + eol)
 	os.Exit(errorExitCode)
 }
