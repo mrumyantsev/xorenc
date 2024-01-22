626640e857a31cb6cbf95c0be81fad22071556c0
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-01-22 14:36:20 -0300

    refactor: shorten error constants, shorten error wrapping function

--- encrypt-file.go
+++ encrypt-file.go
@@ -6,11 +6,11 @@ import (
 )
 
 const (
-	errorExecReadingSeq     = "could not execute reading sequence"
-	errorExecOverwritingSeq = "could not execute overwriting sequence"
-	errorOpenFile           = "could not open file"
-	errorReadFile           = "could not read from file"
-	errorWriteFile          = "could not write to file"
+	errExecReadingSeq     = "could not execute reading sequence"
+	errExecOverwritingSeq = "could not execute overwriting sequence"
+	errOpenFile           = "could not open file"
+	errReadFile           = "could not read from file"
+	errWriteFile          = "could not write to file"
 )
 
 // Encrypt performs per-bit XOR encryption of file, specified in path,
@@ -29,14 +29,14 @@ const (
 func EncryptFile(path string, key []byte, nWorkers int) (nBytes int, err error) {
 	data, err := readFile(path)
 	if err != nil {
-		return 0, decorateError(errorExecReadingSeq, err)
+		return 0, wrapError(errExecReadingSeq, err)
 	}
 
 	Encrypt(data, key, nWorkers)
 
 	err = overwriteFile(path, data)
 	if err != nil {
-		return 0, decorateError(errorExecOverwritingSeq, err)
+		return 0, wrapError(errExecOverwritingSeq, err)
 	}
 
 	return len(data), nil
@@ -47,13 +47,13 @@ func EncryptFile(path string, key []byte, nWorkers int) (nBytes int, err error) 
 func readFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, decorateError(errorOpenFile, err)
+		return nil, wrapError(errOpenFile, err)
 	}
 	defer f.Close()
 
 	data, err := io.ReadAll(f)
 	if err != nil {
-		return nil, decorateError(errorReadFile, err)
+		return nil, wrapError(errReadFile, err)
 	}
 
 	return data, nil
@@ -65,13 +65,13 @@ func readFile(path string) ([]byte, error) {
 func overwriteFile(path string, data []byte) error {
 	f, err := os.OpenFile(path, os.O_TRUNC|os.O_WRONLY, 0)
 	if err != nil {
-		return decorateError(errorOpenFile, err)
+		return wrapError(errOpenFile, err)
 	}
 	defer f.Close()
 
 	_, err = f.Write(data)
 	if err != nil {
-		return decorateError(errorWriteFile, err)
+		return wrapError(errWriteFile, err)
 	}
 
 	return nil
--- lib.go
+++ lib.go
@@ -4,10 +4,10 @@ import "errors"
 
 const errorInsert string = ". error: "
 
-// decorateError decorates a given error with the description, inserting
+// wrapError wraps a given error with the description, inserting
 // ". error: " text in between. If the error is nil, is cause panic.
 // Returns the new error, which text is decorated by the specified
 // description.
-func decorateError(desc string, err error) error {
+func wrapError(desc string, err error) error {
 	return errors.New(desc + errorInsert + err.Error())
 }
