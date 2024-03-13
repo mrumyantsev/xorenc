69acedb793da30516d8acd2c5fbcae5c93ce05b3
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-03-14 00:56:54 +0300

    refactor(cmd/xor): remake functions and variables

--- cmd/xor/main.go
+++ cmd/xor/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"io"
-	"io/fs"
 	"os"
 	"runtime"
 	"strconv"
@@ -14,91 +13,85 @@ import (
 )
 
 const (
-	errorExitCode int    = 1
-	appMark       string = "xor: "
-	usageInfo     string = "Usage:\n  - xor <filepath> <enckey>\n" +
+	appMark   = "xor: "
+	usageInfo = "Usage:\n  - xor <filepath> <enckey>\n" +
 		"  - cat ./def_file | xor <enckey> > ./out_file\n" +
 		"  - xor <enckey> < ./def_file > ./out_file\n\n" +
 		"The encryption key (<enckey>) may contain spaces.\n\n" +
 		"The decryption is provided by repeating of encryption\n" +
 		"with the same key, that was used for encryption."
-	space string = " "
-	eol   string = "\n"
+	space = " "
+	eol   = "\n"
+
+	errorExitCode = 1
 )
 
 var (
-	flagArgs      []string
-	stdinFileInfo fs.FileInfo
-	stdinFileMode fs.FileMode
-	nCores        int = runtime.NumCPU()
-	err           error
+	cliArgs  []string
+	cpuCores = runtime.NumCPU()
 )
 
 func init() {
 	flag.Parse()
 
-	flagArgs = flag.Args()
+	cliArgs = flag.Args()
+}
 
-	stdinFileInfo, err = os.Stdin.Stat()
+func main() {
+	stdinFileInfo, err := os.Stdin.Stat()
 	if err != nil {
 		printError("could not get stdin file info", err)
 
 		os.Exit(errorExitCode)
 	}
 
-	stdinFileMode = stdinFileInfo.Mode()
-}
-
-func main() {
-	// Encrypt the data from stdin to stdout.
-
-	if (stdinFileMode & os.ModeCharDevice) == 0 {
-		if len(flagArgs) < 1 {
-			printUsage("encryption key parameter does not presents")
-
-			os.Exit(errorExitCode)
-		}
-
-		var (
-			encryptKey []byte = []byte(strings.Join(flagArgs, space))
-			stdinData  []byte
-		)
-
-		stdinData, err = io.ReadAll(os.Stdin)
-		if err != nil {
-			printError("could not read data from stdin", err)
-
-			os.Exit(errorExitCode)
-		}
-
-		xor.Encrypt(stdinData, encryptKey, nCores)
-
-		_, err = os.Stdout.Write(stdinData)
-		if err != nil {
-			printError("could not write encrypted data to stdout", err)
-
-			os.Exit(errorExitCode)
-		}
+	if (stdinFileInfo.Mode() & os.ModeCharDevice) == 0 {
+		encryptStdinData()
 
 		return
 	}
 
-	// Encrypt the file, given in file path parameter.
+	encryptFile()
+}
 
-	if len(flagArgs) < 2 {
+func encryptStdinData() {
+	if len(cliArgs) < 1 {
+		printUsage("encryption key parameter does not presents")
+
+		os.Exit(errorExitCode)
+	}
+
+	encryptKey := []byte(strings.Join(cliArgs, space))
+
+	stdinData, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		printError("could not read data from stdin", err)
+
+		os.Exit(errorExitCode)
+	}
+
+	xor.Encrypt(stdinData, encryptKey, cpuCores)
+
+	if _, err = os.Stdout.Write(stdinData); err != nil {
+		printError("could not write encrypted data to stdout", err)
+
+		os.Exit(errorExitCode)
+	}
+}
+
+func encryptFile() {
+	if len(cliArgs) < 2 {
 		printUsage("file path or encryption key is missing " +
 			"in parameters")
 
 		os.Exit(errorExitCode)
 	}
 
-	var (
-		filePath   string = flagArgs[0]
-		encryptKey []byte = []byte(strings.Join(flagArgs[1:], space))
-		encBytes   int
-	)
+	filePath := cliArgs[0]
 
-	encBytes, err = xor.EncryptFile(filePath, encryptKey, nCores)
+	encryptKey := []byte(strings.Join(cliArgs[1:], space))
+
+	encBytes, err := xor.EncryptFile(filePath, encryptKey, cpuCores)
 	if err != nil {
 		printError("could not encrypt file", err)
 
