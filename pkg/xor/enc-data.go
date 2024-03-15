0f0beb25d8ea9c331e2e62a13a78d6cabdcaa424
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-03-15 13:24:39 +0300

    refactor(pkg/xor): remake workers number accessors and optimize Encrypt function

--- pkg/xor/enc-data.go
+++ pkg/xor/enc-data.go
@@ -1,30 +1,42 @@
 package xor
 
 import (
+	"runtime"
 	"sync"
 )
 
-// Encrypt performs per-bit XOR encryption of data by the key.
+const (
+	// Default number of workers that used by data encrypter.
+	NWorkersDefault = 0
+)
+
+var (
+	nWorkers = 0
+)
+
+// Encrypt performs per-bit XOR encryption of data by the symbolic key.
 //
 // The decryption is provided by repeating of encryption with the same
 // key, that was used for encryption.
 //
 // The data processing operation in this function is processed with the
 // workers, which divides the data to the equal pieces and encrypts
-// each. The number of workers is capped to the length of the data, and
-// it makes no processing, if given 0 or less.
-func Encrypt(data []byte, key []byte, nWorkers int) {
-	if (data == nil) || (key == nil) || (nWorkers <= 0) {
-		return
-	}
-
+// each. The number of workers is capped to the length of the data.
+//
+// If the number of workers is set to 0, then the number of workers
+// will be equal to the number of physical processor cores.
+func Encrypt(data []byte, key []byte) {
 	var (
-		dataLen int = len(data)
-		keyLen  int = len(key)
+		dataLen = len(data)
+		keyLen  = len(key)
 	)
 
 	if (dataLen == 0) || (keyLen == 0) {
 		return
+	}
+
+	if nWorkers <= 0 {
+		nWorkers = runtime.NumCPU()
 	}
 
 	if nWorkers > dataLen {
@@ -32,28 +44,30 @@ func Encrypt(data []byte, key []byte, nWorkers int) {
 	}
 
 	var (
-		dataPart  int = dataLen / nWorkers
-		dataStart int = 0
-		dataEnd   int = dataPart
-		keyStart  int = 0
-		i         int = 1 // loop counter
-
-		wg sync.WaitGroup = sync.WaitGroup{}
+		dataPart  = dataLen / nWorkers
+		dataStart = 0
+		dataEnd   = dataPart
+		keyStart  = 0
+		i         = 1 // loop counter
+		wg        = &sync.WaitGroup{}
 	)
 
 	wg.Add(nWorkers)
 
 	for {
-		// starting current worker
+		// Starting current worker.
 
-		go worker(workerData{
+		w := &worker{
 			data:      data,
 			key:       key,
+			keyLen:    keyLen,
 			keyStart:  keyStart,
 			dataStart: dataStart,
 			dataEnd:   dataEnd,
-			wg:        &wg,
-		})
+			wg:        wg,
+		}
+
+		go w.do()
 
 		if i == nWorkers {
 			break
@@ -61,7 +75,7 @@ func Encrypt(data []byte, key []byte, nWorkers int) {
 
 		i++
 
-		// calculating next worker data
+		// Calculating next worker data.
 
 		dataStart += dataPart
 		keyStart += dataPart
@@ -80,33 +94,44 @@ func Encrypt(data []byte, key []byte, nWorkers int) {
 	wg.Wait()
 }
 
-func worker(wd workerData) {
-	var (
-		keyLen  int = len(wd.key)
-		dataLen int = wd.dataEnd
-		i       int = wd.dataStart // data index
-		j       int = wd.keyStart  // key index
-	)
-
-	for i < dataLen {
-		if j >= keyLen {
-			j = 0
-		}
-
-		wd.data[i] = wd.data[i] ^ wd.key[j]
-
-		i++
-		j++
-	}
-
-	wd.wg.Done()
+// NWorkers gets current number of workers that used to encrypt data
+// inside Encrypt function.
+func NWorkers() int {
+	return nWorkers
 }
 
-type workerData struct {
+// SetNWorkers sets how many workers will encrypt data inside Encrypt
+// function.
+func SetNWorkers(num int) {
+	nWorkers = num
+}
+
+type worker struct {
 	data      []byte
 	key       []byte
+	keyLen    int
 	keyStart  int
 	dataStart int
 	dataEnd   int
 	wg        *sync.WaitGroup
+}
+
+func (w *worker) do() {
+	defer w.wg.Done()
+
+	var (
+		i = w.dataStart // data index
+		j = w.keyStart  // key index
+	)
+
+	for i < w.dataEnd {
+		if j >= w.keyLen {
+			j = 0
+		}
+
+		w.data[i] = w.data[i] ^ w.key[j]
+
+		i++
+		j++
+	}
 }
--- pkg/xor/enc-file.go
+++ pkg/xor/enc-file.go
@@ -11,28 +11,29 @@ const (
 )
 
 // EncryptFile performs per-bit XOR encryption of file, specified in
-// path, by the key.
+// path, by the symbolic key.
 //
 // The decryption is provided by repeating of encryption with the same
 // key, that was used for encryption.
 //
 // The data processing operation in this function is processed with the
 // workers, which divides the data to the equal pieces and encrypts
-// each. The number of workers is capped to the length of the data, and
-// it makes no processing, if given 0 or less.
+// each. The number of workers is capped to the length of the data.
+//
+// If the number of workers is set to 0, then the number of workers
+// will be equal to the number of physical processor cores.
 //
 // Returns the number of the encrypted bytes and the error with its
 // description.
-func EncryptFile(path string, key []byte, nWorkers int) (nBytes int, err error) {
+func EncryptFile(path string, key []byte) (nBytes int, err error) {
 	data, err := fileops.ReadFile(path)
 	if err != nil {
 		return 0, e.Wrap(errCompleteFileReading, err)
 	}
 
-	Encrypt(data, key, nWorkers)
+	Encrypt(data, key)
 
-	err = fileops.OverwriteFile(path, data)
-	if err != nil {
+	if err = fileops.OverwriteFile(path, data); err != nil {
 		return 0, e.Wrap(errCompleteFileOverwriting, err)
 	}
 
